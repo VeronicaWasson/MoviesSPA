17683c2c965f82790d10a65f77e305521deda089
MikeWasson <mike@mikewasson.com>
2013-09-06 16:45:27 -0700

    Initial checkin - Movie SPA sample

---
+++ MovieSPA/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieSPA.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+    }
+}
---
+++ MovieSPA/Models/MovieSPAContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieSPA.Models
+{
+    public class MovieSPAContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MovieSPAContext() : base("name=MovieSPAContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MovieSPA.Models.Movie> Movies { get; set; }
+    
+    }
+}
