cc7a5bc1c50e33aa96e85e49974c549a75bf0fb5
MikeWasson <mike@mikewasson.com>
2013-09-10 14:07:23 -0700

    Update for RC1

--- MovieSPA/Global.asax.cs
+++ MovieSPA/Global.asax.cs
@@ -11,7 +11,7 @@ namespace MovieSPA
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
